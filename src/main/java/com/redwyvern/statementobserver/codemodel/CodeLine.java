5612eba255c26707719af26aa41cea864efe01b8
Nick Weedon <nick@weedon.org.au>
2019-02-22 07:59:05 -0500

    Migrated codemodel classes

---
+++ src/main/java/com/redwyvern/statementobserver/codemodel/CodeLine.java
@@ -0,0 +1,28 @@
+package com.redwyvern.statementobserver.codemodel;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class CodeLine implements Comparable {
+    private final ClassMethodCode classMethodCode;
+    private final String code;
+    private final int index;
+    private final int lineNumber;
+    private Statement statement;
+
+
+
+    @Override
+    public int compareTo(Object o) {
+
+        if(!(o instanceof CodeLine)) {
+            throw new RuntimeException("CodeLine is not comparable to class of type '" + o.getClass() + "'");
+        }
+        CodeLine rhs = (CodeLine) o;
+        return index - rhs.index;
+    }
+}
